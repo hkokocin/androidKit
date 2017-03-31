917768af6a512e2c2602571998bcfce446488671
Hendrik Kokocinski <hendrik.kokocinski@weltn24.de>
2017-03-31 12:51:09 +0200

    fix kit getter

--- library/src/main/java/com/github/hkokocin/androidkit/AndroidKit.kt
+++ library/src/main/java/com/github/hkokocin/androidkit/AndroidKit.kt
@@ -4,13 +4,16 @@ import com.github.hkokocin.androidkit.content.ContextKit
 import com.github.hkokocin.androidkit.view.ViewKit
 import com.github.hkokocin.androidkit.widget.WidgetKit
 
-val kit = AndroidKit.instance
+val kit: AndroidKit
+    get() = AndroidKit.instance
 
 class AndroidKit private constructor()
     : WidgetKit, ViewKit, ContextKit {
 
     companion object {
         var instance = AndroidKit()
-        fun resetToDefault(){ instance = AndroidKit() }
+        fun resetToDefault() {
+            instance = AndroidKit()
+        }
     }
 }
