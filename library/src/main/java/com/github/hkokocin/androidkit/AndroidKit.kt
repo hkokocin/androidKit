f72983a2e2f9e73ab6cb1803fb5b1e956a5cc487
Hendrik Kokocinski <hendrik.kokocinski@weltn24.de>
2017-04-25 08:41:13 +0200

    wrap basic functions into BaseKit for testability

--- library/src/main/java/com/github/hkokocin/androidkit/AndroidKit.kt
+++ library/src/main/java/com/github/hkokocin/androidkit/AndroidKit.kt
@@ -1,6 +1,7 @@
 package com.github.hkokocin.androidkit
 
 import com.github.hkokocin.androidkit.app.ActivityKit
+import com.github.hkokocin.androidkit.app.BaseKit
 import com.github.hkokocin.androidkit.content.ContextKit
 import com.github.hkokocin.androidkit.view.ViewKit
 import com.github.hkokocin.androidkit.widget.WidgetKit
@@ -9,7 +10,7 @@ import com.github.hkokocin.androidkit.widget.WidgetKit
 //    get() = AndroidKit.instance
 
 class AndroidKit private constructor()
-    : WidgetKit, ViewKit, ContextKit, ActivityKit {
+    : BaseKit, WidgetKit, ViewKit, ContextKit, ActivityKit {
 
     companion object {
         var instance = AndroidKit()
